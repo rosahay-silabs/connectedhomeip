afb45f5eb0bdb30058b9f43d41b14132c7c0ff39
rosahay-silabs <Rohan.Sahay@silabs.com>
2023-04-13 17:19:42 +0530

    Revert MY_USART to USART0

--- examples/platform/silabs/efr32/spi_multiplex.c
+++ examples/platform/silabs/efr32/spi_multiplex.c
@@ -52,7 +52,7 @@ void spi_drv_reinit(uint32_t baudrate)
     usartInit.databits               = (USART_Databits_TypeDef) databits;
     usartInit.autoCsEnable           = true;
 
-    USART_InitSync(MY_USART, &usartInit);
+    USART_InitSync(USART0, &usartInit);
 }
 
 /****************************************************************************
