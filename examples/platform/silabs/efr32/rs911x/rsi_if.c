b0130b12977d595762b61045cb72a59cb932e997
rosahay-silabs <rohan.sahay@silabs.com>
2023-05-19 16:57:38 +0530

    Refactor wfx_lwip_set_sta_link_up

--- examples/platform/silabs/efr32/rs911x/rsi_if.c
+++ examples/platform/silabs/efr32/rs911x/rsi_if.c
@@ -627,7 +627,10 @@ void wfx_rsi_task(void * arg)
                 /* Checks if the assigned IPv6 address is preferred by evaluating
                  * the first block of IPv6 address ( block 0)
                  */
-                SILABS_LOG("%s: checking ip6_addr_ispreferred", __func__);
+                if (!hasNotifiedIPV6)
+                {
+                    SILABS_LOG("%s: checking ip6_addr_ispreferred", __func__);
+                }
                 if ((ip6_addr_ispreferred(netif_ip6_addr_state(sta_netif, 0))) && !hasNotifiedIPV6)
                 {
                     SILABS_LOG("%s: success ip6_addr_ispreferred", __func__);
@@ -644,11 +647,6 @@ void wfx_rsi_task(void * arg)
         }
         if (flags & WFX_EVT_STA_START_JOIN)
         {
-#if (CHIP_DEVICE_CONFIG_ENABLE_IPV4)
-            hasNotifiedIPV4 = false;
-#endif /* CHIP_DEVICE_CONFIG_ENABLE_IPV4 */
-            hasNotifiedIPV6             = false;
-            hasNotifiedWifiConnectivity = false;
             // saving the AP related info
             wfx_rsi_save_ap_info();
             // Joining to the network
