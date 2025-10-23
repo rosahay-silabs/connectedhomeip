979bd5e5f00faf4bd3810b77cc89f1f359083c08
Rohan Sahay <103027015+rosahay-silabs@users.noreply.github.com>
2025-10-24 00:32:01 +0530

    Refactor SPI multiplexing functions to improve clarity and remove unused declarations
    align the BAUDRATE with SDK provided speed
    
    Refactor SPI multiplexing documentation for clarity and consistency
    
    Update based on `silabs_board` instead of `silabs_family`
    
    Update the path for file headers as per coding standards

--- src/platform/silabs/wifi/ncp/spi_multiplex.h
+++ src/platform/silabs/wifi/ncp/spi_multiplex.h
@@ -60,26 +60,6 @@ extern "C" {
  * @return returns void
  *****************************************************************************/
 void SPIDRV_SetBaudrate(uint32_t);
-
-#if defined(SL_MATTER_SIWX_WIFI_ENABLE)
-/****************************************************************************
- * @fn  sl_status_t sl_wfx_host_spi_cs_assert()
- * @brief
- *     Assert chip select.
- * @param[in] None
- * @return returns SL_STATUS_OK
- *****************************************************************************/
-sl_status_t sl_wfx_host_spi_cs_assert(void);
-
-/****************************************************************************
- * @fn  sl_status_t sl_wfx_host_spi_cs_deassert()
- * @brief
- *     De-Assert chip select.
- * @param[in] None
- * @return returns SL_STATUS_OK
- *****************************************************************************/
-sl_status_t sl_wfx_host_spi_cs_deassert(void);
-#endif /* SL_MATTER_SIWX_WIFI_ENABLE */
 #endif // SL_SPICTRL_MUX
 
 #if SL_MX25CTRL_MUX
