a2a0c557f63d473d0d51e6285fcccc5dd863f3e2
rosahay-silabs <103027015+rosahay-silabs@users.noreply.github.com>
2026-06-10 03:15:31 +0530

    Update lwipopts-siwx.h to standardize TCP_MSS and PBUF_POOL_BUFSIZE configurations, enhancing compatibility and performance.

--- src/lwip/silabs/lwipopts-siwx.h
+++ src/lwip/silabs/lwipopts-siwx.h
@@ -71,14 +71,12 @@
 
 // Setting the priority of the lwip thread to osPriorityAboveNormal
 #define TCPIP_THREAD_PRIO (32)
+#define TCP_MSS (1460)
 
 #ifdef SL_MATTER_ENABLE_AWS
 #define LWIP_DNS 1
 #define DNS_RAND_TXID() ((u32_t) rand())
-#define TCP_MSS (4 * 1152)
-#else
-#define LWIP_DNS 0
-#define TCP_MSS (1152)
+#define TCP_WND (4 * TCP_MSS)
 #endif /* SL_MATTER_ENABLE_AWS */
 
 #define LWIP_FREERTOS_USE_STATIC_TCPIP_TASK 1
@@ -144,7 +142,7 @@
 #define LWIP_PBUF_FROM_CUSTOM_POOLS (0)
 #define MEMP_USE_CUSTOM_POOLS (0)
 #define PBUF_POOL_SIZE (32)
-#define PBUF_POOL_BUFSIZE (1280) // IPv6 path MTU
+#define PBUF_POOL_BUFSIZE (1520) // TCP_MSS + 40 + PBUF_LINK_ENCAPSULATION_HLEN + PBUF_LINK_HLEN
 #define PBUF_CUSTOM_POOL_IDX_START (MEMP_PBUF_POOL_SMALL)
 #define PBUF_CUSTOM_POOL_IDX_END (MEMP_PBUF_POOL_LARGE)
 
@@ -155,7 +153,7 @@
 #define SUB_ETHERNET_HEADER_SPACE (0)
 #define PBUF_LINK_HLEN (14)
 
-#define TCPIP_THREAD_STACKSIZE (2048)
+#define TCPIP_THREAD_STACKSIZE (2 * 1024)
 
 #define NETIF_MAX_HWADDR_LEN 8U
 
